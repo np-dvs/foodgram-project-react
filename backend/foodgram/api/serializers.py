a2344c009b1cf9cc4f1340140fd88c9260c9a95b
np-dvs <nastyapan@gmail.com>
2024-03-13 21:42:45 +0300

    wtf_2

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -194,7 +194,7 @@ class RecipeReadSerializer(serializers.ModelSerializer):
         user = self.context['request'].user
         if user.is_anonymous:
             return False
-        return user.favourites.filter(recipe=obj).exists()
+        return user.favorites.filter(recipe=obj).exists()
 
     def get_is_in_shopping_cart(self, obj):
         user = self.context['request'].user
